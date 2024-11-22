df4ac07a5acebe8097e0075f35da6056040f1115
rictusempra52 <rictusempra52@gmail.com>
2024-11-22 23:19:32 +0900

    gridjsに配列を渡すように変更

--- table.js
+++ table.js
@@ -1,13 +1,27 @@
-const grid = new gridjs.Grid({
-    columns: ['Name', 'Email'],
-    data: [
+window.onload = (e) => {
+console.log("ページ読み込み完了")
+
+    let theading = ["名前", "出欠1"]
+    let data = [
         ["John", "john@example.com"],
         ["Mark", "mark@example.com"],
         ["Eoin", "eoin@example.com"],
         ["Sarah", "sarahcdd@example.com"],
         ["Afshin", "afshin@example.com"]
     ]
-});
+    makegrid(theading, data)
 
-//grid.renderの引数をjqueryで指定する場合には、.get(0)をつける(DOMそのものを指定する必要があるため)
-grid.render($(".history-area").get(0));
+    // 見出しとデータを与えて表を出力する関数
+    function makegrid(heading, data) {
+        console.log("表の生成開始")
+        const grid = new gridjs.Grid({
+            sort: true,
+            columns: heading,
+            data: data
+        });
+
+        //grid.renderの引数をjqueryで指定する場合には、.get(0)をつける(DOMそのものを指定する必要があるため)
+        grid.render($(".history-area").get(0));
+    }
+
+}
