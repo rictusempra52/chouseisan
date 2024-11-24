1a7e1d3b60d88ff156747426d5044f295a5e4a0e
rictusempra52 <rictusempra52@gmail.com>
2024-11-24 11:50:02 +0900

    調整日付を今日以降に設定する処理をスマートにした

--- js/init.js
+++ js/init.js
@@ -3,23 +3,9 @@
 $(function () {
 
     // input[type="date"]のminとmaxを現在日時取得して指定
-    // min (現在の3日後)
     var minD = new Date();// 現在日時取得
-
-    var minYear = minD.getFullYear();// 上書きしたので、年月日それぞれ取得
-
-    var minMonth = minD.getMonth() + 1;
-    if (minMonth < 10) {// 2桁にしないといけないので 10未満なら頭に0を追加
-        var minMonth = '0' + minMonth;
-    }
-
-    var minDate = minD.getDate();
-    if (minDate < 10) {// 2桁にしないといけないので 10未満なら頭に0を追加
-        var minDate = '0' + minDate;
-    }
-
-    // yyyy-mm-ddになるようにまとめる
-    var min = minYear + '-' + minMonth + '-' + minDate;
+    //minDをYYYY-MM-DDに整形
+    var min = minD.toISOString().split('T')[0];
     // min属性を設定
     $('input[type ="date"]').attr('min', min);
 
