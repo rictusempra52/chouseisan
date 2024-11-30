7222b3fba698f49800bcdeb179fd6a9b5f3e8e83
rictusempra52 <rictusempra52@gmail.com>
2024-11-30 19:00:32 +0900

    コメント振った

--- js/init.js
+++ js/init.js
@@ -1,92 +1,103 @@
 
 //データの定義
+/**
+ * 出欠ステータスを管理する定数オブジェクト
+ * 
+ * @constant {Object} attendance - 出欠の状態を表す定数オブジェクト
+ * @property {number} not_decided - 未定 (0)
+ * @property {number} presence - 出席 (1)
+ * @property {number} absence - 欠席 (2)
+ */
 const attendance = {
+    not_decided: 0, // 未定
+    presence: 1,    // 出席
+    absence: 2,     // 欠席
+};
 
-    not_decided: 0,
-    presence: 1,
-    absence: 2,
-}
-
-let tableheading = ["名前", "出席番号", "出欠1"]
-let data = [
+// let tableheading = ["名前", "出席番号", "出欠1"]
+/**
+ * 学生の出欠データリスト
+ * 
+ * @type {Array<Object>} data - 学生ごとの出欠情報を保持する配列
+ * @property {string} name - 学生の名前
+ * @property {string} [stu_num] - 学生番号
+ * @property {number} parti_1 - 出欠の状態 (attendance 定数を使用)
+ */
+let member_data = [
     {
         name: "ゆき",
         stu_num: "TW01",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "きみしー",
         stu_num: "TW02",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "りょーすけ",
         stu_num: "TW03",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "なつき",
         stu_num: "TW04",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "ふみや",
         stu_num: "TW05",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "あべちゃん",
         stu_num: "TW06",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "かがやん",
         stu_num: "TW07",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "かっしー",
-        stu_num: "TW08",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "ひろし",
-        stu_num: "TW09",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "たろー",
-        stu_num: "TW10",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "かわたつ",
-        stu_num: "TW11",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "こすげ",
-        stu_num: "TW12",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "ほそかわ",
-        stu_num: "TW13",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "ぺんぎん",
-        stu_num: "TW14",
-        parti_1: attendance.not_decided
+        parti_1: attendance.not_decided // 出欠: 未定
     },
     {
         name: "らんこ",
-        stu_num: "TW15",
-        parti_1: attendance.not_decided
-    }]
+        parti_1: attendance.not_decided // 出欠: 未定
+    }
+];
 
-// 合計人数を表す定数
-const headcount = data.length
+
+/**
+ *合計人数を表す定数
+   */
+const headcount = member_data.length
 
 // input[type="date"]のminとmaxを現在日時取得して指定
 //今日の日付をYYYY-MM-DDに整形
@@ -97,21 +108,20 @@ $('input[type ="date"]').attr('value', today);
 console.log(today + "を初期値に設定");
 
 
-
 window.onload = (e) => {
     console.log("ページ読み込み完了")
 
     // すべての「出欠1」列にチェックボックスを挿入する
     for (let i = 0; i < headcount; i++) {
-        data[i][2] = checkbox(i, "participation", false)
+        member_data[i][2] = checkbox(i, "participation", false)
     } console.log("☑を挿入完了");
 
 
     // 合計人数を最下列に表示
-    data.push(["合計人数：" + headcount, ""])
+    member_data.push(["合計人数：" + headcount, ""])
 
     // 表を出力
-    make_grid_on_history_area(tableheading, data);
+    make_grid_on_history_area(tableheading, member_data);
 
     // 見出しとデータを与えると表を出力する関数
     function make_grid_on_history_area(heading, data) {
