36480c199a42ff68269b251d194320f35004ff14
rictusempra52 <rictusempra52@gmail.com>
2024-12-01 00:11:38 +0900

    ☑をボタンに変更した

--- js/init.js
+++ js/init.js
@@ -14,7 +14,7 @@ const attendance = {
     absence: 2,     // 欠席
 };
 
-// let tableheading = ["名前", "出席番号", "出欠1"]
+let tableheading = ["名前", "出席番号", "出欠1"]
 /**
  * 学生の出欠データリスト
  * 
@@ -93,6 +93,8 @@ let member_data = [
     }
 ];
 
+let table_html = [[]]
+
 
 /**
  *合計人数を表す定数
@@ -108,20 +110,28 @@ $('input[type ="date"]').attr('value', today);
 console.log(today + "を初期値に設定");
 
 
-window.onload = (e) => {
+window.onload = () => {
     console.log("ページ読み込み完了")
 
     // すべての「出欠1」列にチェックボックスを挿入する
     for (let i = 0; i < headcount; i++) {
-        member_data[i][2] = checkbox(i, "participation", false)
-    } console.log("☑を挿入完了");
+
+        // もしtable_html[i]（i番目の行）がまだ存在していない場合、新しく空の配列を作る
+        // → この配列は行ごとに情報を入れるための箱として使う
+        if (!table_html[i]) table_html[i] = [];
+
+        // i番目の行の3列目（インデックス2）にボタンを作成して格納
+        table_html[i][2] = chousei_button(i, "participation", member_data[i].parti_1);
+        console.log(table_html[i][2]);
+    }
+    console.log("日程調整用ボタン挿入完了");
 
 
     // 合計人数を最下列に表示
     member_data.push(["合計人数：" + headcount, ""])
 
     // 表を出力
-    make_grid_on_history_area(tableheading, member_data);
+    make_grid_on_history_area(tableheading, table_html);
 
     // 見出しとデータを与えると表を出力する関数
     function make_grid_on_history_area(heading, data) {
@@ -140,18 +150,34 @@ window.onload = (e) => {
 
     }
 
-    // 表の中にチェックボックスを挿入する関数
-    function checkbox(id, name, checked) {
-        const tmp = "<input type='checkbox' id=" + id + " name=" + name
-        switch (checked) {
-            case true:
-                return gridjs.html(tmp + " checked />");
-                break;
-
-            default:
-                return gridjs.html(tmp + " />");
-                break;
+    /**
+     * 表の中に日程変更用のボタンを挿入する関数
+     * 
+     * @function chousei_button
+     * @param {string} id - ボタンの一意のID
+     * @param {string} name - ボタンの名前属性
+     * @param {number} status - ボタンの初期状態を示す番号 (0: 未定, 1: 出席, 2: 欠席) 0,1,2 以外の場合は 0 ("未定") になります。
+     * @returns {string} - 状態変更用ボタンのHTML要素を表す文字列 (gridjs の html ラッパーを使用)
+     * 
+     * @description
+     * この関数は、指定された状態 (`status`) に基づいてボタンを生成します。
+     * 状態は "未定", "出席", "欠席" の3種類があり、対応するラベルがボタンの `value` 属性として設定されます。
+     * 生成されたボタンは grid.js の `html` メソッドを使用してテーブルに挿入することが想定されています。
+     * 
+     * @example
+     * // 状態が "出席" のボタンを作成
+     * const buttonHtml = chousei_button("btn1", "attendance", 1);
+     * // => <input type='button' id='btn1' name='attendance' value='出席' />
+     */
+    function chousei_button(id, name, status) {
+        // status が 0, 1, 2 以外の場合は 0 ("未定") にする
+        if (![0, 1, 2].includes(status)) {
+            status = 0;
         }
-    }
 
+        const txt = ["未定", "出席", "欠席"]; // 各状態に対応するテキスト
+        const tmp = "<input type='button' id='" + id + "' name='" + name + "' value='" + txt[status] + "' />";
+        return gridjs.html(tmp);
+    }
 }
+
